c1e853d1757aefc4df0d1ab582d9692af7db4638
daniel kostin <dikostin@edu.hse.ru>
2024-12-02 03:23:48 +0300

    devide main and handlers

--- src/bot/keyboards/keyboards.py
+++ src/bot/keyboards/keyboards.py
@@ -1,42 +1,56 @@
 # keyboards.py
-from aiogram.types import ReplyKeyboardMarkup, KeyboardButton, InlineKeyboardMarkup, InlineKeyboardButton, LabeledPrice
-
-def main_menu_keyboard():
-    keyboard = ReplyKeyboardMarkup(resize_keyboard=True)
-    keyboard.add(KeyboardButton("Получить Ключ"))
-    keyboard.add(KeyboardButton("Управление ключами"))
-    return keyboard
-
-# Создаем объекты инлайн-кнопок
-get_key = InlineKeyboardButton(
-    text='•	Получить Ключ',
-    callback_data='get_keys_pressed'
+from aiogram.types import (
+    ReplyKeyboardMarkup,
+    KeyboardButton,
+    InlineKeyboardMarkup,
+    InlineKeyboardButton,
 )
 
-ket_management = InlineKeyboardButton(
-    text='•	Управление ключами',
-    callback_data='key_management_pressed'
-)
 
-main_menu_keyboard = InlineKeyboardMarkup(
-    inline_keyboard=[[get_key],
-                     [ket_management]]
-)
+def get_main_menu_keyboard():
+    # Создаем объекты инлайн-кнопок
+    get_key = InlineKeyboardButton(
+        text="Получить Ключ", callback_data="get_keys_pressed"
+    )
 
-# Кнопки для выбора периода
-month_button = InlineKeyboardButton(text='• Месяц (1$)', callback_data='1_month')
-three_month_button = InlineKeyboardButton(text='• 3 Месяца (10$)', callback_data='3_months')
-six_month_button = InlineKeyboardButton(text='• 6 Месяцев (100$)', callback_data='6_months')
-year_button = InlineKeyboardButton(text='• 12 Месяцев (1000$)', callback_data='12_months')
+    ket_management = InlineKeyboardButton(
+        text="Управление ключами", callback_data="key_management_pressed"
+    )
 
-period_keyboard = InlineKeyboardMarkup(
-    inline_keyboard=[[month_button],
-                     [three_month_button],
-                     [six_month_button],
-                     [year_button]]
-)
+    return InlineKeyboardMarkup(inline_keyboard=[[get_key], [ket_management]])
+
+
+def get_period_keyboard():
+    # Кнопки для выбора периода
+    month_button = InlineKeyboardButton(text="1 Месяц (1$)", callback_data="1_month")
+    three_month_button = InlineKeyboardButton(
+        text="3 Месяца (10$)", callback_data="3_months"
+    )
+    six_month_button = InlineKeyboardButton(
+        text="6 Месяцев (100$)", callback_data="6_months"
+    )
+    year_button = InlineKeyboardButton(
+        text="12 Месяцев (1000$)", callback_data="12_months"
+    )
+
+    return InlineKeyboardMarkup(
+        inline_keyboard=[
+            [month_button],
+            [three_month_button],
+            [six_month_button],
+            [year_button],
+        ]
+    )
+
 
 def get_installation_button():
-    return InlineKeyboardMarkup(inline_keyboard=[
-        [InlineKeyboardButton(text="Нужна инструкция по установке", callback_data="installation_instructions")]
-    ])
+    return InlineKeyboardMarkup(
+        inline_keyboard=[
+            [
+                InlineKeyboardButton(
+                    text="Нужна инструкция по установке",
+                    callback_data="installation_instructions",
+                )
+            ]
+        ]
+    )
--- src/database/user_db.py
+++ src/database/user_db.py
@@ -4,10 +4,11 @@ from sqlalchemy.orm import relationship, declarative_base, sessionmaker
 # Создаем базовый класс для моделей
 Base = declarative_base()
 
+
 class DbProcessor:
     def __init__(self):
         # Создаем движок для подключения к базе данных
-        self.engine = create_engine('sqlite:///vpn_users.db', echo=True)
+        self.engine = create_engine("sqlite:///vpn_users.db", echo=True)
         # Создаем сессию для работы с базой данных
         self.Session = sessionmaker(bind=self.engine)
 
@@ -16,24 +17,23 @@ class DbProcessor:
 
     # Определение таблицы Users
     class User(Base):
-        __tablename__ = 'users'
+        __tablename__ = "users"
 
         user_telegram_id = Column(String, primary_key=True)  # Telegram ID пользователя
         subscription_status = Column(String)  # Статус подписки (active/inactive)
         use_trial_period = Column(Boolean)  # Использован ли пробный период
 
         # Отношение с таблицей Keys (один ко многим)
-        keys = relationship('Key', back_populates='user', cascade="all, delete-orphan")
-
+        keys = relationship("Key", back_populates="user", cascade="all, delete-orphan")
 
     # Определение таблицы Keys
     class Key(Base):
-        __tablename__ = 'keys'
+        __tablename__ = "keys"
 
         key_id = Column(String, primary_key=True)  # Сам ключ для VPN
-        user_telegram_id = Column(String, ForeignKey('users.user_telegram_id'))  # ID пользователя (ссылка на пользователя)
+        user_telegram_id = Column(
+            String, ForeignKey("users.user_telegram_id")
+        )  # ID пользователя (ссылка на пользователя)
 
         # Обратное отношение к таблице Users
-        user = relationship('User', back_populates='keys')
-
-
+        user = relationship("User", back_populates="keys")
