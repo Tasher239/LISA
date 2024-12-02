e0ca4f457d205588be3f9bfd5c223acc599739c1
asadullinam <144450818+asadullinam@users.noreply.github.com>
2024-12-02 17:12:49 +0300

    Доработана база данных и добавлен новый функционал

--- src/database/user_db.py
+++ src/database/user_db.py
@@ -1,4 +1,4 @@
-from sqlalchemy import create_engine, Column, String, Integer, Boolean, ForeignKey
+from sqlalchemy import Column, String, Integer, Boolean, ForeignKey, DateTime, create_engine, Table, MetaData
 from sqlalchemy.orm import relationship, declarative_base, sessionmaker
 
 # Создаем базовый класс для моделей
@@ -9,31 +9,29 @@ class DbProcessor:
     def __init__(self):
         # Создаем движок для подключения к базе данных
         self.engine = create_engine("sqlite:///vpn_users.db", echo=True)
-        # Создаем сессию для работы с базой данных
         self.Session = sessionmaker(bind=self.engine)
 
-        # Создание всех таблиц в базе данных (если они еще не существуют)
+    def init_db(self):
+        """Синхронная инициализация базы данных."""
         Base.metadata.create_all(self.engine)
 
     # Определение таблицы Users
     class User(Base):
         __tablename__ = "users"
-
         user_telegram_id = Column(String, primary_key=True)  # Telegram ID пользователя
         subscription_status = Column(String)  # Статус подписки (active/inactive)
         use_trial_period = Column(Boolean)  # Использован ли пробный период
-
         # Отношение с таблицей Keys (один ко многим)
         keys = relationship("Key", back_populates="user", cascade="all, delete-orphan")
 
     # Определение таблицы Keys
     class Key(Base):
         __tablename__ = "keys"
-
         key_id = Column(String, primary_key=True)  # Сам ключ для VPN
         user_telegram_id = Column(
             String, ForeignKey("users.user_telegram_id")
         )  # ID пользователя (ссылка на пользователя)
-
         # Обратное отношение к таблице Users
         user = relationship("User", back_populates="keys")
+        expiration_date = Column(DateTime)
+        start_date = Column(DateTime)
