6b1e2944d0f464d3d76f3626ce54ba72a739b3e7
daniel kostin <dikostin@edu.hse.ru>
2024-12-04 01:55:07 +0300

    upd bd

--- src/bot/utils/send_message.py
+++ src/bot/utils/send_message.py
@@ -4,7 +4,11 @@ from logger.logging_config import setup_logger
 from bot.keyboards.keyboards import (
     get_installation_button,
 )
+from bot.initialization.bot_init import bot
+
 from aiogram.fsm.context import FSMContext
+from bot.keyboards.keyboards import get_prodlit_keyboard
+from bot.lexicon.lexicon import Notification
 
 logger = setup_logger()
 
@@ -23,3 +27,20 @@ async def send_message_and_save(message, text, state: FSMContext, **kwargs):
     sent_message = await message.answer(text, **kwargs)
     await state.update_data(previous_message_id=sent_message.message_id)
     return sent_message
+
+
+async def send_message_subscription_expired(user):
+    await bot.send_message(
+        user.user_telegram_id,
+        Notification.SUBSCRIPTION_EXPIRED,
+        parse_mode="HTML",
+        reply_markup=get_prodlit_keyboard(),  # Клавиатура для продления подписки
+    )
+
+async def send_message_subscription_ends(user):
+    await bot.send_message(
+        user.user_telegram_id,
+        Notification.SUBSCRIPTION_REMINDER,
+        parse_mode="HTML",
+        reply_markup=get_prodlit_keyboard()
+    )
