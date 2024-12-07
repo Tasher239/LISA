1c350b819606ef60cb9a5807e194fe19091c6c57
asadullinam <144450818+asadullinam@users.noreply.github.com>
2024-12-07 23:28:43 +0300

    52

--- src/bot/initialization/outline_processor_init.py
+++ src/bot/initialization/outline_processor_init.py
@@ -1,7 +1,9 @@
 import os
+import sys
+sys.path.append('/Users/aydar/Desktop/VPN2_2/lib/python3.11/site-packages')
 from outline_vpn.outline_vpn import OutlineVPN
 from dotenv import load_dotenv
-from bot.utils.outline_processor import OutlineProcessor
+from LISA.src.bot.utils.outline_processor import OutlineProcessor
 
 load_dotenv()
 
