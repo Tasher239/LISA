46f44286e281b705066439fc8ebd2fafa2acafa8
asadullinam <144450818+asadullinam@users.noreply.github.com>
2024-12-07 23:28:43 +0300

    52

--- src/logger/logging_config.py
+++ src/logger/logging_config.py
@@ -1,13 +1,15 @@
 import logging
 import sys
+import os
 
+LOG_FILE_PATH = os.path.abspath(os.path.join(os.path.dirname(__file__), "bot.log"))
 
 def setup_logger():
     logging.basicConfig(
         level=logging.INFO,
         format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
         handlers=[
-            logging.FileHandler("logger/bot.log"),
+            logging.FileHandler(LOG_FILE_PATH),
             logging.StreamHandler(sys.stdout),
         ],
     )
