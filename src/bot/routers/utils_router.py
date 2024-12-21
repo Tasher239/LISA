9e9aaadf9978bb95d833fccf6bc9cfdb048af27d
daniel kostin <dikostin@edu.hse.ru>
2024-12-21 16:05:39 +0300

    52

--- src/bot/routers/utils_router.py
+++ src/bot/routers/utils_router.py
@@ -1,30 +1,31 @@
-import os
-from dotenv import load_dotenv
-
 from aiogram.types import CallbackQuery, LabeledPrice
 from aiogram import F, Router
 from aiogram.fsm.context import FSMContext
 from aiogram.fsm.state import default_state
+from aiogram.filters import StateFilter
 
+from bot.lexicon.lexicon import INFO
 from bot.utils.string_makers import get_instruction_string
-from bot.keyboards.keyboards import get_back_button
+from bot.keyboards.keyboards import (
+    get_back_button,
+    get_main_menu_keyboard,
+    get_about_us_keyboard,
+)
+from bot.fsm.states import MainMenu
 
 from logger.logging_config import setup_logger
-
-load_dotenv()
-provider_token = os.getenv("PROVIDER_SBER_TOKEN")
 
 router = Router()
 logger = setup_logger()
 
 
+# фильтр запроса инструкции
 @router.callback_query(F.data == "installation_instructions")
 async def send_installation_instructions(callback: CallbackQuery, state: FSMContext):
-    # Пример инструкции
     data = await state.get_data()
     key_access_url = data.get("key_access_url", "URL не найден")
     instructions = get_instruction_string(key_access_url)
-    await callback.message.answer(
+    await callback.message.edit_text(
         instructions,
         parse_mode="Markdown",
         disable_web_page_preview=True,
@@ -33,3 +34,23 @@ async def send_installation_instructions(callback: CallbackQuery, state: FSMCont
 
     await callback.answer()
     await state.set_state(default_state)
+
+
+# фильтр кнопки в главное меню из любого места
+@router.callback_query(F.data == "back_to_main_menu")
+async def back_button(callback: CallbackQuery, state: FSMContext):
+    await state.set_state(default_state)
+    # await delete_previous_message(callback.message.chat.id, state)
+    await callback.message.edit_text(
+        "Вы вернулись в главное меню. Выберите, что вы хотите сделать",
+        reply_markup=get_main_menu_keyboard(),
+    )
+    await state.set_state(MainMenu.waiting_for_action)
+    await callback.answer()
+
+
+@router.callback_query(StateFilter(MainMenu.about_us), F.data != "back_to_main_menu")
+async def show_about_us(callback: CallbackQuery, state: FSMContext):
+    await callback.message.edit_text(
+        INFO.ABOUT_US, reply_markup=get_about_us_keyboard(), parse_mode="Markdown"
+    )
--- src/bot/utils/send_message.py
+++ src/bot/utils/send_message.py
@@ -1,10 +1,10 @@
+from outline_vpn.outline_vpn import OutlineKey
+
 from aiogram.fsm.context import FSMContext
 from aiogram.types import Message
 
-from src.bot.keyboards.keyboards import get_installation_button
-
 from src.bot.initialization.bot_init import bot
-from src.bot.keyboards.keyboards import get_prodlit_keyboard
+from src.bot.keyboards.keyboards import get_prodlit_keyboard, get_installation_button
 from src.bot.lexicon.lexicon import Notification
 from src.bot.utils.string_makers import get_your_key_string
 
@@ -13,17 +13,19 @@ from src.logger.logging_config import setup_logger
 logger = setup_logger()
 
 
-async def send_key_to_user(message: Message, key, text="Ваш ключ от VPN"):
-    """Отправляет созданный ключ пользователю."""
+async def send_key_to_user(
+    message: Message, key: OutlineKey, text: str = "Ваш ключ от VPN"
+) -> None:
+    """Отправляет ключ пользователю."""
     logger.info(f"Key created: {key} for user {message.from_user.id}")
-    await message.answer(
+    await message.edit_text(
         get_your_key_string(key, text),
         parse_mode="Markdown",
         reply_markup=get_installation_button(),
     )
 
 
-async def send_message_and_save(message, text, state: FSMContext, **kwargs):
+async def send_message_and_save(message, text: str, state: FSMContext, **kwargs):
     sent_message = await message.answer(text, **kwargs)
     await state.update_data(previous_message_id=sent_message.message_id)
     return sent_message
