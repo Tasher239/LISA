d1a70aea6534e16b81a9b20877f475b82036a1bd
Daniel Kostin <dikostin@edu.hse.ru>
2025-01-29 17:47:00 +0300

    52

--- src/bot/routers/choice_vpn_type_router.py
+++ src/bot/routers/choice_vpn_type_router.py
@@ -3,16 +3,16 @@ import os
 from aiogram import F, Router
 from aiogram.fsm.context import FSMContext
 from aiogram.types import CallbackQuery, LabeledPrice
-from dotenv import load_dotenv
+from aiogram.filters import StateFilter
 
-from bot.fsm.states import GetKey
+from bot.fsm.states import GetKey, ManageKeys
 from bot.keyboards.keyboards import get_choice_vpn_type_keyboard
 from logger.logging_config import setup_logger
 
 router = Router()
 logger = setup_logger()
 
-
+@router.callback_query(StateFilter(ManageKeys.no_active_keys), F.data.in_(["get_keys_pressed"]))
 @router.callback_query(F.data.in_(["choice_vpn_type", "back_to_choice_vpn_type"]))
 async def choice_vpn_type(callback: CallbackQuery, state: FSMContext):
     await state.set_state(GetKey.choosing_vpn_protocol_type)
