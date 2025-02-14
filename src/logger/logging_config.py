6b415c29691c732a81adae2e4d88b70b86fdb905
asadullinam <amasadullin@edu.hse.ru>
2025-02-14 17:23:57 +0300

    Начало работы по добавление сервера

--- src/logger/logging_config.py
+++ src/logger/logging_config.py
@@ -8,7 +8,7 @@ LOG_FILE_PATH = os.path.abspath(os.path.join(os.path.dirname(__file__), "bot.log
 def setup_logger():
     logging.basicConfig(
         level=logging.INFO,
-        format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+        format="%(asctime)s - %(name)s - %(levelname)s - %(filename)s:%(lineno)d - %(message)s",
         handlers=[
             logging.FileHandler(LOG_FILE_PATH),
             logging.StreamHandler(sys.stdout),
